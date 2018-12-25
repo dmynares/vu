83e52b3b8af5f2439160f5428a552c0826885fc2
dmynares <43295716+dmynares@users.noreply.github.com>
2018-12-26 04:29:25 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,10 +1,7 @@
 <?php
 $config = [
-	"558ca0ba-99a8-4857-9c53-7e8d345be145",
-	"558ca0ba-99a8-4857-9c53-7e8d345be145",
-	"558ca0ba-99a8-4857-9c53-7e8d345be145",
-	"558ca0ba-99a8-4857-9c53-7e8d345be145",
+	"87a0084e-889a-49f7-a236-4323f98a923b",#dmdrivestorage05@gmail.com
+	"dfc462fc-c7aa-4c9b-ac61-e79ed567e771",#joseuaurelio221991@gmail.com
+	];
 
-
-
-];
+	
